f887bd0d2c8c012654b409638784db9c2c6e462d
Benjamin383 <brieucau@gmail.com>
2024-10-03 15:59:44 +0200

    Customer List Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {CustomerListComponent} from "./components/customers/customer-list/customer-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path: 'customers', component: CustomerListComponent},
+];
